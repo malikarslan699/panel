ee15948c2ce71793b7289f85f052014e6e01449b
malikarslan699 <46280359+malikarslan699@users.noreply.github.com>
2024-11-07 11:54:07 +0400

    Update setup_connect.sh

--- setup_connect.sh
+++ setup_connect.sh
@@ -46,8 +46,22 @@ echo -e "# MySQL credentials\nDB_HOST=\"$DB_HOST\"\nDB_USER=\"$DB_USER\"\nDB_PAS
 echo "MySQL credentials have been successfully added to the file."
 
 # Step 3: Downloading vpn.php
+VPN_PHP_PATH="/var/www/html/vpn.php"
+if [ -f "$VPN_PHP_PATH" ]; then
+    sudo rm "$VPN_PHP_PATH"  # Remove existing file if it exists
+fi
+wget -O "$VPN_PHP_PATH" https://raw.githubusercontent.com/malikarslan699/panel/main/vpn.php
+mv vpn.php /var/www/html/
+sudo chmod +x "$VPN_PHP_PATH"
 
 # Step 4: Downloading connect.sh
+CONNECT_SH_PATH="/etc/ocserv/scripts/connect.sh"
+if [ -f "$CONNECT_SH_PATH" ]; then
+    sudo rm "$CONNECT_SH_PATH"  # Remove existing file if it exists
+fi
+wget -O "$CONNECT_SH_PATH" https://raw.githubusercontent.com/malikarslan699/panel/main/connect.sh
+mv connect.sh /etc/ocserv/scripts/
+sudo chmod +x "$CONNECT_SH_PATH"
 
 # Step 5: Edit sudoers file for www-data permissions
 SUDO_FILE="/etc/sudoers"
